b8d40ac0f9a418a2398fa6ce5d394f5ecafb8bd3
riyadh <riyadh02220@gmail.com>
2019-09-08 14:39:58 +0200

    boxify the main view

--- components/search.js
+++ components/search.js
@@ -4,15 +4,19 @@ import {StyleSheet, View, Button, TextInput, Slider} from "react-native";
 class Search extends React.Component {
     render() {
         return (
-            <View>
-                <TextInput style={style.textInput} placeholder="tap a movie title"></TextInput>
+            <View style={style.container}>
                 <Button title="Search" onPress={() => {}}></Button>
+                <TextInput style={style.textInput} placeholder="tap a movie title"></TextInput>
             </View>
         );
     }
 }
 
 const style= StyleSheet.create({
+    container: {
+        flex:1,
+        justifyContent:"flex-end",
+    },
     textInput: {
         height: 40,
         margin: 5,
