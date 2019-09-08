1699166d4f3284d27facf5cd25c6c582c60619a7
riyadh <riyadh02220@gmail.com>
2019-09-08 13:55:38 +0200

    add stylesheet to the search component

--- components/search.js
+++ components/search.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import {View, Button, TextInput} from "react-native";
+import {StyleSheet, View, Button, TextInput, Slider} from "react-native";
 
 class Search extends React.Component {
     render() {
         return (
             <View>
-                <TextInput placeholder="tap a movie title"></TextInput>
+                <TextInput style={style.textInput} placeholder="tap a movie title"></TextInput>
                 <Button title="Search" onPress={() => {}}></Button>
             </View>
         );
     }
 }
+
+const style= StyleSheet.create({
+    textInput: {
+        height: 40,
+        margin: 5,
+        padding: 5,
+    },
+    SearchButton: {
+        
+    }
+})
+
 
 export default Search;
