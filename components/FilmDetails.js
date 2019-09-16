6195bbf2a6ba642f35d48a66cde6aeb8952eb523
riyadh <riyadh02220@gmail.com>
2019-09-16 11:02:54 +0200

    more details view styled

--- components/FilmDetails.js
+++ components/FilmDetails.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyleSheet, View, Text, ActivityIndicator, ScrollView, Image } from 'react-native'
-import {getMoviesDetails} from '../API/movieDB';
+import {getMoviesDetails, getPoster} from '../API/movieDB';
 
 
 class FilmDetail extends React.Component {
@@ -40,19 +40,14 @@ class FilmDetail extends React.Component {
         <ScrollView style={styles.scrollview_container}>
           <Image
           style={styles.image}
-          source={{uri: 'https://image.tmdb.org/t/p/w300'+ this.state.film.backdrop_path}}
+          source={{uri: getPoster(this.state.film.backdrop_path)}}
           />
-          <Text>{this.state.film.title}</Text>
-          <Text>{this.state.film.overview}</Text>
-          <Text>{this.state.film.release_date}</Text>
-          <Text>{this.state.film.vote_average}</Text>
-          <Text>{this.state.film.vote_count}</Text>
-          <Text>{this.state.film.budget}</Text>
-          <Text>{this.state.film.type}</Text>
-          <Text>
-            
-          </Text>
-          <Text>{this.state.film.production_companies.map(item => item.name).join(" / ")}</Text>
+          <Text style={styles.title_text}>{this.state.film.title}</Text>
+          <Text style={styles.description_text}>Overview : {this.state.film.overview}</Text>
+          <Text style={styles.default_text}>Released in : {this.state.film.release_date}</Text>
+          <Text style={styles.default_text}>rate : {this.state.film.vote_average}</Text>
+          <Text style={styles.default_text}>genres : {this.state.film.genres.map(data => data.name).join(" / ")}</Text>
+          <Text style={styles.default_text}>companies : {this.state.film.production_companies.map(item => item.name).join(" / ")}</Text>
         </ScrollView>
       )
     }
@@ -82,6 +77,37 @@ const styles = StyleSheet.create({
     bottom: 0,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  scrollview_container: {
+    flex: 1,
+    margin:0
+  },
+  image: {
+    height: 169,
+    margin: 5
+  },
+  title_text: {
+    fontWeight: 'bold',
+    fontSize: 35,
+    flex: 1,
+    flexWrap: 'wrap',
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10,
+    marginBottom: 10,
+    color: '#000000',
+    textAlign: 'center'
+  },
+  description_text: {
+    fontStyle: 'italic',
+    color: '#666666',
+    margin: 5,
+    marginBottom: 15
+  },
+  default_text: {
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 5,
   }
 })
 
