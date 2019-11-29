48c90e7d2d30205b3e3cc4d4aa4339b6d41442fd
riyadh <riyadh02220@gmail.com>
2019-11-29 08:26:44 +0100

    user actions persisted

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import Search from './components/search'
-import Navigation from './Navigation/Navigation'
 import { Provider } from 'react-redux'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/es/integration/react'
+
 import Store from './Store/configureStore'
+import Navigation from './Navigation/Navigation'
 
 export default class App extends React.Component {  
   render() {
+    let persiststore = persistStore(Store)
     return (
       <Provider store={Store}>
-        <Navigation/>
+        <PersistGate persistor={persiststore}>
+          <Navigation/>
+        </PersistGate>
       </Provider>
     )
   }
--- Store/configureStore.js
+++ Store/configureStore.js
@@ -1,9 +1,17 @@
 import { createStore, combineReducers } from 'redux';
+import { persistCombineReducers } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
 import toggleFavorite from './reducers/favoritereducer'
 import manageHistoricFilms from './reducers/historicReducer'
 import setAvatar from './reducers/avatarReducer'
 
-const reducers = combineReducers({
+const rootPersistConfig = {
+    key:'root',
+    storage: storage
+}
+
+const reducers = persistCombineReducers(rootPersistConfig, {
     toggleFavorite,
     manageHistoricFilms,
     setAvatar
